63d21e918ffa001e84dd5471a07518c9580a4854
zsk <zsk526@qq.com>
2017-10-28 00:12:40 +0800

    Modify template

--- src/index.js
+++ src/index.js
@@ -1,17 +1,12 @@
 import transformImage from './transform.js';
 
-const image = require('./img/template-02.png');
-const mask = require('./img/mask-02.png');
+// const image = require('http://qn-us.fenglinghudong.com/editor-shoes/1/plate-02.png');
+// const mask = require('http://qn-us.fenglinghudong.com/editor-shoes/1/mask-02.png');
 
 window.transform = transformImage({
-    imageSrc: image,
-    maskSrc: mask,
-    points: [
-        { x: 0, y: 0 },
-        { x: 892, y: 92 },
-        { x: 863, y: 977 },
-        { x: 26, y: 862 },
-    ], // 默认位置
+    imageSrc: 'http://qn-us.fenglinghudong.com/editor-shoes/3/plate-02.png',
+    maskSrc: 'http://qn-us.fenglinghudong.com/editor-shoes/3/mask-03.png',
+    points: [{"x":-88,"y":112},{"x":913,"y":73},{"x":820,"y":697},{"x":228,"y":830}], // 默认位置
     extend: 2, // 扩展三角形，处理缝隙问题
     hasDrag: true, // 开启拖拽∏
     hasDot: true, // 显示点
