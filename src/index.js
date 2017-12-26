fc66fb2dfa43f1ad93cfccd520a3bbf5ad83d4f8
zsk <zsk526@qq.com>
2017-12-26 23:49:55 +0800

    Modify image

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 import transformImage from './transform.js';
 
-const image = require('./editor-shoes/5/plate-02.png');
-const mask = require('./editor-shoes/5/mask-01-02.png');
+// const image = require('./editor-shoes/5/plate-02.png');
+// const mask = require('./editor-shoes/5/mask-01-02.png');
 
 window.transform = transformImage({
-    imageSrc: image, //'http://qn-us.fenglinghudong.com/editor-shoes/5/plate-03.png',
-    maskSrc: mask, //'http://qn-us.fenglinghudong.com/editor-shoes/5/mask-01-03.png',
+    imageSrc: 'http://qn-us.fenglinghudong.com/editor-shoes/11/plate-01.png',
+    maskSrc: 'http://qn-us.fenglinghudong.com/editor-shoes/11/1/mask-01.png',
     points: [{"x":97,"y":618},{"x":654,"y":373},{"x":831,"y":854},{"x":192,"y":947}], // 默认位置
     extend: 2, // 扩展三角形，处理缝隙问题
     hasDrag: true, // 开启拖拽∏
     hasDot: true, // 显示点
-    hasRect: true, // 显示方格
+    hasRect: false, // 显示方格
     hasPic: true, // 显示图片
     count: 10, // 等分割数量
 });
